e3f7ed4ab1e0744dc8f3baf13f70875850348ccd
jodosjodos <jeandedieu2030@gmail.com>
2023-08-12 21:43:17 +0200

    removing some bugs from backend

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import express from "express"
 import * as dotenv from "dotenv"
 import { mainRouter } from "./routers/main.js"
+import cors from "cors"
 dotenv.config()
 
 const app=express()
 app.use(express.json())
 
+app.use(cors())
 app.use("/api/auth",mainRouter)
 
 
--- controllers/main.js
+++ controllers/main.js
@@ -1,9 +1,9 @@
 import * as dotenv from "dotenv";
-
 import * as nodemailer from "nodemailer";
+
 dotenv.config();
 
-export const sendEmail = (req, res) => {
+export const sendEmail = async (req, res) => {
   try {
     const { name, email, message } = req.body;
 
@@ -14,37 +14,27 @@ export const sendEmail = (req, res) => {
         pass: process.env.EMAIL_PASSWORD,
       },
     };
-    // templeate t0  send email
+
     const htmlTemplate = `
       <h1 style="color:#0D3167;">This is a message from user called   <span style="text-decoration: underline;">${name}</span>
-      with   this email : ${email}  who send it from your portifolio</h1>
+      with   this email : ${email}  who send it from your portfolio</h1>
       <p>${message}</p>
     `;
 
-    const transpoter = nodemailer.createTransport(configuration);
+    const transporter = nodemailer.createTransport(configuration);
     const options = {
       from: email,
       to: process.env.EMAIL,
-      template: "email",
-      subject: "email sent by user from my portifolio",
+      subject: "Email sent by user from my portfolio",
       html: htmlTemplate,
     };
 
-    transpoter.sendMail(options, (err, info) => {
-      if (err) {
-        console.log(err.message);
-        return res.status(503).json({ err: err });
-      } else {
-        if (info) {
-          console.log(info);
-          return res
-            .status(200)
-            .json({ msg: "your email has been sent successfully " });
-        }
-      }
-    });
-  } catch (error) {
-    console.log(error.message);
-    return res.status(500).json({ err: error.message });
+    const info = await transporter.sendMail(options);
+    return res
+      .status(200)
+      .json({ msg: "Your email has been sent successfully" });
+  } catch (err) {
+    console.log(err.message);
+    return res.status(503).json({ err: err.message });
   }
 };
