dfcccd81fd15cb02ea68487e047c386fdead3bcf
jodosjodos <jeandedieu2030@gmail.com>
2023-08-13 14:09:35 +0200

    handling social media

--- src/components/SocialMedia.tsx
+++ src/components/SocialMedia.tsx
@@ -1,17 +1,33 @@
-import {AiFillGithub, AiFillLinkedin, AiFillTwitterSquare} from "react-icons/ai"
-import {FaDiscord} from "react-icons/fa"
+import {
+  AiFillGithub,
+  AiFillLinkedin,
+  AiFillTwitterSquare,
+} from "react-icons/ai";
+import { FaDiscord } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const SocialMedia = () => {
   return (
     <div className="absolute left-5  top-[20em]">
       <div className="flex flex-col bg-white border-2  px-5 py-2  gap-16">
-        <AiFillLinkedin size={40}  />
-        <AiFillTwitterSquare size={40}/>
-        <AiFillGithub size={40}/>
-        <FaDiscord size={40}/>
+        <Link
+          to="https://www.linkedin.com/in/jean-de-dieu-nshimyumukiza-97b315259/"
+          target="_blank"
+        >
+          <AiFillLinkedin size={40} />
+        </Link>
+        <Link to="https://twitter.com/JodosJodos1" target="_blank">
+          <AiFillTwitterSquare size={40} />
+        </Link>
+        <Link to="https://github.com/jodosjodos" target="_blank">
+          <AiFillGithub size={40} />
+        </Link>
+        <Link to="https://discord.com/users/1054299820615028746" target="_blank">
+          <FaDiscord size={40} />
+        </Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SocialMedia
+export default SocialMedia;
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,6 +1,22 @@
+import { Link, useNavigate } from "react-router-dom";
+
 const About = () => {
+  const Navigate = useNavigate();
+
+  const scrollToSection = (id: string) => {
+    if (id === "introduction") {
+      Navigate("/");
+    } else {
+      const element = document.getElementById(id);
+      if (element) {
+        element.scrollIntoView({ behavior: "smooth" });
+        Navigate(`#${id}`);
+      }
+    }
+  };
+
   return (
-    <div >
+    <div>
       <div className="flex pb-40 pt-10 border-b-2 border-black  flex-col gap-8">
         <div className="flex  justify-center items-center font-medium font-kelly-slab text-2xl text-text-primary ">
           <h1 className="border-b-4 border-[#0D3167]">ABOUT ME</h1>
@@ -14,10 +30,14 @@ const About = () => {
         </div>
         <div className="flex flex-row justify-around">
           <div className="flex flex-col gap-8">
-            <h1 className="text-2xl font-bold text-text-primary">Get know me! </h1>
+            <h1 className="text-2xl font-bold text-text-primary">
+              Get know me!{" "}
+            </h1>
             <div className="flex flex-col gap-1 ">
               <p>
-                I'm a  <span className="font-bold">Full Stack Web Developer</span> with a strong focus on JavaScript
+                I'm a{" "}
+                <span className="font-bold">Full Stack Web Developer</span> with
+                a strong focus on JavaScript
               </p>
               <p>
                 ,php, python, c technologies. My expertise lies in building and
@@ -35,26 +55,49 @@ const About = () => {
                 Data structure and algorithmns in python.You can explore some of
                 my
               </p>
-              <p className="self-center"> projects in the  <span className="font-bold ">Projects section.</span></p>
+              <p className="self-center">
+                {" "}
+                projects in the{" "}
+                <span className="font-bold ">Projects section.</span>
+              </p>
               <p>
                 As an enthusiast for knowledge sharing, I love providing
                 valuable content
               </p>
               <p>
-                related to  <span className="font-bold">Web Development</span> and Programming, aiming to help
-                fellow
+                related to <span className="font-bold">Web Development</span>{" "}
+                and Programming, aiming to help fellow
               </p>
               <p>
                 developers in the Dev Community. You can find and connect with
                 me on
               </p>
               <p>
-                 <span className="font-bold text-text-primary text-xl border-b-2 border-[#0D3167]">LinkedIn,</span> and you can check some my codes on <span className="font-bold text-text-primary text-xl border-b-2 border-[#0D3167]">Github</span> where I
+                <Link
+                  className="font-bold text-text-primary text-xl border-b-2 border-[#0D3167]"
+                  to="https://www.linkedin.com/in/jean-de-dieu-nshimyumukiza-97b315259/"
+                  target="_blank"
+                >
+                  LinkedIn,
+                </Link>{" "}
+                and you can check some my codes on{" "}
+                <Link
+                  className="font-bold text-text-primary text-xl border-b-2 border-[#0D3167]"
+                  to="https://github.com/jodosjodos"
+                  target="_blank"
+                >
+                  Github
+                </Link>{" "}
+                where I
               </p>
-              <p className="self-center"> regularly post informative content and codes.</p>
+              <p className="self-center">
+                {" "}
+                regularly post informative content and codes.
+              </p>
               <p>
-                I am currently open to <span className="font-bold">job opportunities</span> where I can contribute
-                my
+                I am currently open to{" "}
+                <span className="font-bold">job opportunities</span> where I can
+                contribute my
               </p>
               <p>
                 skills, continue learning, and grow professionally. If you have
@@ -65,12 +108,21 @@ const About = () => {
                 hesitate to
               </p>
               <p>
-                reach out to me in <span className="font-bold"> Contact section.</span> I'm excited to explore
-                potential
+                reach out to me in{" "}
+                <span className="font-bold"> Contact section.</span> I'm excited
+                to explore potential
               </p>
-              <p className="self-center"> opportunities and be a valuable asset to your team.</p>
+              <p className="self-center">
+                {" "}
+                opportunities and be a valuable asset to your team.
+              </p>
             </div>
-            <button className=" bg-text-primary text-white font-kelly-slab w-48 py-2 rounded-md text-xl self-center ">Contact me</button>
+            <button
+              className=" bg-text-primary text-white font-kelly-slab w-48 py-2 rounded-md text-xl self-center "
+              onClick={() => scrollToSection("contact")}
+            >
+              Contact me
+            </button>
           </div>
           <div className="flex flex-col gap-8">
             <h1 className="text-2xl font-bold text-text-primary">My skills</h1>
@@ -87,7 +139,7 @@ const About = () => {
               <div className=" bg-[#D9D9D9] rounded-md h-10 w-24  text-text-primary flex items-center justify-center">
                 <p>react</p>
               </div>
-              <div className=" bg-[#D9D9D9] rounded-md h-10 w-24  text-text-primary flex items-center justify-center" >
+              <div className=" bg-[#D9D9D9] rounded-md h-10 w-24  text-text-primary flex items-center justify-center">
                 <p>vue</p>
               </div>
               <div className=" bg-[#D9D9D9] rounded-md h-10 w-24  text-text-primary flex items-center justify-center">
